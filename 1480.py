b2067857076ad8b3a3d6b7b447ba5402d78a1240
MohammadEhsan Akhavanpour <akhavanpour.ehsan@gmail.com>
2022-10-25 11:11:47 -0400

    Running Sum of 1d Array

---
+++ 1480.py
@@ -0,0 +1,25 @@
+# Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+from typing import List
+
+
+# Complexity Analysis
+#
+# Time complexity: O(n)
+# where n is the length of the input array.
+# This is because we use a single loop that iterates over the entire array to calculate the running sum.
+#
+# Space complexity: O(1)
+# since we don't use any additional space to find the running sum.
+# Note that we do not take into consideration the space occupied by the output array.
+
+def runningSum(nums: List[int]) -> List[int]:
+
+    for i in range(1, len(nums)):
+        nums[i] += nums[i - 1]
+
+    return nums
+
+
+if __name__ == '__main__':
+    nums = [1, 2, 3, 4]
+    print(runningSum(nums))
